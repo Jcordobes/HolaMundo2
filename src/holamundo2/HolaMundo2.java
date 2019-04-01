a0f3dde3d053b6055e7a52b1ce84c35911413a0a
Jcordobes <jcordobescarmona@outlook.es>
2019-04-01 12:16:10 +0200

    Modificado todo

---
+++ src/holamundo2/HolaMundo2.java
@@ -0,0 +1,16 @@
+package holamundo2;
+
+/**
+ *
+ * @author usuario
+ */
+public class HolaMundo2 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde el programa 2");
+	System.out.println("Abril 2019");
+	System.out.println("IES Luis Velez De Guevara");
+    }
+    
+}
